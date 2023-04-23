8a9dc81348adb1399a2429b837a05778258842da
Jappy0 <pengyao.ping@smail.uts.edu.au>
2023-04-23 12:05:15 +1000

    fix bug

--- src/noise2read/__init__.py
+++ src/noise2read/__init__.py
@@ -2,6 +2,6 @@
 # @Author: Pengyao Ping
 # @Date:   2023-01-24 15:47:23
 # @Last Modified by:   Pengyao Ping
-# @Last Modified time: 2023-04-06 12:41:39
+# @Last Modified time: 2023-04-23 11:19:38
 
-__version__ = "0.0.73"
+__version__ = "0.0.75"
