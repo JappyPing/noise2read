60a89782f1c3af5debaa582c49ed1b0887594748
Jappy0 <pengyao.ping@smail.uts.edu.au>
2023-04-27 17:51:43 +1000

    docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,7 +2,7 @@
 # @Author: Pengyao Ping
 # @Date:   2023-02-16 11:02:09
 # @Last Modified by:   Pengyao Ping
-# @Last Modified time: 2023-04-27 17:38:55
+# @Last Modified time: 2023-04-27 17:51:05
 # Configuration file for the Sphinx documentation builder.
 import os
 import sys
@@ -90,6 +90,11 @@ html_theme_options = {
         "color-brand-content": "#E5B62F",
     },
 }
+html_context = {
+    'css_files': [
+        '_static/custom.css',  # replace with your CSS file path
+    ],
+}
 
 html_theme_options: Dict[str, Any] = {
     "footer_icons": [
@@ -97,7 +102,7 @@ html_theme_options: Dict[str, Any] = {
             "name": "GitHub",
             "url": "https://github.com/Jappy0/noise2read",
             "html": """
-                <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 20 20">
+                <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 16 16">
                     <path fill-rule="evenodd" d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0 0 16 8c0-4.42-3.58-8-8-8z"></path>
                 </svg>
             """,
