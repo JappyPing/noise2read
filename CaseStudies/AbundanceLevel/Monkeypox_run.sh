88d2867ae2a7ada5c042b6295361971f918d361e
Jappy0 <pengyao.ping@smail.uts.edu.au>
2023-05-03 01:44:17 +1000

    fix casestudies bug

--- CaseStudies/AbundanceLevel/Monkeypox_run.sh
+++ CaseStudies/AbundanceLevel/Monkeypox_run.sh
@@ -2,10 +2,10 @@
 # @Author: Pengyao Ping
 # @Date:   2023-04-27 23:04:36
 # @Last Modified by:   Pengyao Ping
-# @Last Modified time: 2023-04-28 09:51:35
+# @Last Modified time: 2023-05-03 01:25:29
 
 virus_name="Monkeypox"                                                 # the name of the virus
-ref="./D18_D21/ref/GCA_025947495.1_ASM2594749v1_genomic.fasta"                               # the reference with full path
+ref="./D18_D21/Monkeypox/ref/GCA_025947495.1_ASM2594749v1_genomic.fasta"                               # the reference with full path
 raw_r1="./D18_D21/Monkeypox/raw/D20_SRR22085311_1.fastq"              # original read 1
 raw_r2="./D18_D21/Monkeypox/raw/D21_SRR22085311_2.fastq"              # original read 2
 correct_r1="./D18_D21/Monkeypox/corrected/D20_SRR22085311_1.fastq"    # corrected read 1
--- CaseStudies/AbundanceLevel/SARS_Cov_2_run.sh
+++ CaseStudies/AbundanceLevel/SARS_Cov_2_run.sh
@@ -2,10 +2,10 @@
 # @Author: Pengyao Ping
 # @Date:   2023-04-27 23:04:36
 # @Last Modified by:   Pengyao Ping
-# @Last Modified time: 2023-04-28 09:51:45
+# @Last Modified time: 2023-05-03 01:25:11
 
 virus_name="SARS_Cov_2"                                                 # the name of the virus
-ref="./D18_D21/ref/sars_cov_ref_MN996528.1.fasta"                               # the reference with full path
+ref="./D18_D21/SARS_Cov_2/ref/sars_cov_ref_MN996528.1.fasta"                               # the reference with full path
 raw_r1="./D18_D21/SARS_Cov_2/raw/D18_SRR11092062_reduced_r1.fastq"              # original read 1
 raw_r2="./D18_D21/SARS_Cov_2/raw/D19_SRR11092062_reduced_r2.fastq"              # original read 2
 correct_r1="./D18_D21/SARS_Cov_2/corrected/D18_SRR11092062_reduced_r1.fastq"    # corrected read 1
