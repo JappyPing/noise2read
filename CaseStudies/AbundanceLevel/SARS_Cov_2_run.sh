a5f26cdee6778d1d0c89d92289a063fec6086bf3
Jappy0 <pengyao.ping@smail.uts.edu.au>
2023-05-03 15:52:11 +1000

    casestudy

--- CaseStudies/AbundanceLevel/SARS_Cov_2_run.sh
+++ CaseStudies/AbundanceLevel/SARS_Cov_2_run.sh
@@ -2,7 +2,7 @@
 # @Author: Pengyao Ping
 # @Date:   2023-04-27 23:04:36
 # @Last Modified by:   Pengyao Ping
-# @Last Modified time: 2023-05-03 15:14:07
+# @Last Modified time: 2023-05-03 15:51:52
 
 virus_name="SARS_Cov_2"                                                 # the name of the virus
 ref="./D18_D21/SARS_Cov_2/ref/sars_cov_ref_MN996528.1.fasta"                               # the reference with full path
@@ -14,8 +14,8 @@ correct_r2="./D18_D21/SARS_Cov_2/corrected/D19_SRR11092062_reduced_r2.fastq"    
 mkdir -p ./result/SARS_Cov_2/raw
 ./get_coverage.sh -r ${ref} -1 ${raw_r1} -2 ${raw_r2} -o ./result/SARS_Cov_2/raw
 
-mkdir -p ./result/SARS_Cov_2/raw
-./get_coverage.sh -r ${ref} -1 ${correct_r1} -2 ${correct_r2} -o ./result/SARS_Cov_2/raw
+mkdir -p ./result/SARS_Cov_2/correct
+./get_coverage.sh -r ${ref} -1 ${correct_r1} -2 ${correct_r2} -o ./result/SARS_Cov_2/correct
 
 raw_cvg="./result/SARS_Cov_2/raw/prn_cvg.txt"
 correct_cvg="./result/SARS_Cov_2/correct/prn_cvg.txt"
