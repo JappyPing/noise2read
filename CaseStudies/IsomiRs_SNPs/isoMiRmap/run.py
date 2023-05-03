407f726a8e9de43af0aeb2ff299b3a174c76eb2b
Jappy0 <pengyao.ping@smail.uts.edu.au>
2023-05-03 15:49:39 +1000

    case study

--- CaseStudies/IsomiRs_SNPs/isoMiRmap/run.py
+++ CaseStudies/IsomiRs_SNPs/isoMiRmap/run.py
@@ -2,7 +2,7 @@
 # @Author: Pengyao Ping
 # @Date:   2022-12-19 18:56:24
 # @Last Modified by:   Pengyao Ping
-# @Last Modified time: 2023-04-30 14:08:09
+# @Last Modified time: 2023-05-03 15:49:03
 import os
 
 raw_dir = "../D22_D31/raw_no_adapters/"
@@ -16,9 +16,9 @@ for file_name in file_names:
 
 correct_dir = "../D22_D31/corrected/"
 
-file_names = os.listdir(correct_dir)
+cor_file_names = os.listdir(correct_dir)
 
-for file_name in file_names:
-    raw = os.path.join(correct_dir, file_name)
-    isomir_prfix = file_name.split(".MI_")[0]
-    os.system("python IsoMiRmap.py %s --p %s" % (raw, isomir_prfix))
+for cor_file_name in cor_file_names:
+    correct = os.path.join(correct_dir, cor_file_name)
+    cor_isomir_prfix = cor_file_name.split("corrected.fastq")[0]
+    os.system("python IsoMiRmap.py %s --p %s" % (correct, cor_isomir_prfix))
